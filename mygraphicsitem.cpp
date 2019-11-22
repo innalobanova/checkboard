2d9c3073243cae139d0953c668bd4a30f55bb4e7
innalobanova <55132370+innalobanova@users.noreply.github.com>
2019-11-22 13:55:50 +0100

    Add files via upload
    replaces the line with a triangle

--- mygraphicsitem.cpp
+++ mygraphicsitem.cpp
@@ -6,6 +6,7 @@
 #include <QRectF>
 #include <QtMath>
 #include <QtDebug>
+#include <QPointF>
 
 MyGraphicsItem::MyGraphicsItem (QGraphicsItem *parent) :
     QGraphicsItem(parent)
@@ -28,7 +29,7 @@ void MyGraphicsItem::paint(QPainter *painter, const QStyleOptionGraphicsItem *op
     int startAngle = -45 * 16;
     int spanAngle = 270 * 16;
     pen.setColor(Qt::blue);
-    pen.setWidth(40);
+    pen.setWidth(5);
     painter->setPen(pen);
     painter->drawArc(40,40,320,320,startAngle,spanAngle);
     xc=200;
@@ -37,10 +38,31 @@ void MyGraphicsItem::paint(QPainter *painter, const QStyleOptionGraphicsItem *op
     xpos=xc+qRound(160*cos((225-v)*pi/180));
     ypos=yc-qRound(160*sin((225-v)*pi/180));
     qDebug()<<(160*cos((225-v)*pi/180.0))<<"\n"<<qRound(160*cos((225-v)*pi/180));
-    pen.setColor(Qt::red);
+   /* pen.setColor(Qt::red);
     pen.setWidth(5);
     painter->setPen(pen);
-    painter->drawLine(xc,yc,xpos,ypos);
+    painter->drawLine(xc,yc,xpos,ypos);*/
+    QBrush brush;
+    pen.setWidth(1);
+    brush.setStyle(Qt::SolidPattern);
+
+
+    pen.setColor(Qt::red);
+    brush.setColor(Qt::red);
+    painter->setPen(pen);
+    painter->setBrush(brush);
+    QPointF points[3] = {
+          QPointF(xc-qRound(7*cos((225-v-90)*pi/180)),yc+qRound(7*sin((225-v-90)*pi/180))),
+          QPointF(xpos, ypos),
+          QPointF(xc+qRound(7*cos((225-v-90)*pi/180)),yc-qRound(7*sin((225-v-90)*pi/180)))
+           };
+
+    painter->drawConvexPolygon(points, 3);
+    pen.setColor(Qt::black);
+    brush.setColor(Qt::black);
+    painter->setPen(pen);
+    painter->setBrush(brush);
+    painter->drawEllipse(xc-15,yc-15,30,30);
 
 
 
